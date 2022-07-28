552c50518149d924394bf9825f6df5efed5ed4e0
negishi-o <abc.x.piko@gmail.com>
2022-07-28 11:30:40 +0900

    first commit

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+// Twigライブラリの読込み
+require_once '../vendor/autoload.php';
+ 
+// Twigを使用するテンプレートの読込み
+$loader = new \Twig\Loader\FilesystemLoader('../view');
+$twig = new \Twig\Environment($loader);
+ 
+// htmlに渡すデータ
+$data = array(
+    'title' => 'MyTitle',
+    'message'  => 'MyMessage',
+);
+ 
+// テンプレートのレンダリング
+echo $twig->render('index.html.twig', $data);
+?>
